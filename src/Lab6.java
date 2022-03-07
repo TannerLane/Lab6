88ceaa91e68e34983141dd7d9b3925db5e17081c
TannerLane <tannerlane@ufl.edu>
2022-03-06 22:34:37 -0500

    Subtraction Version

--- src/Lab6.java
+++ src/Lab6.java
@@ -4,7 +4,7 @@ public class Lab6 {
     public static void main(String[] args) {
 
         Scanner scnr = new Scanner(System.in);
-        int num1, num2, sum;
+        int num1, num2, difference;
 
         System.out.print("Enter number 1: ");
         num1 = scnr.nextInt();
@@ -12,8 +12,8 @@ public class Lab6 {
         System.out.print("Enter number 2: ");
         num2 = scnr.nextInt();
 
-        sum = num1 + num2;
+        difference = num1 - num2;
 
-        System.out.println("\nThe sum is " + sum);
+        System.out.println("\nThe difference is " + difference);
     }
 }
